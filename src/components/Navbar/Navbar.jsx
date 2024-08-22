210653047b311dc4c53993214f5e92d2d611221d
shubham1126 <shubhamprajapati1026@gmail.com>
2024-08-22 17:46:44 +0530

    added navbar logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,43 @@
 // import React from 'react'
+import logo from "/assets/images/logo.png";
 
 const Navbar = () => {
   return (
-    <div className="shadow-sm">
-      <nav className=" container mx-auto flex  justify-between items-center p-4 bg-navbar   ">
-        <h1 className="text-black text-3xl font-semibold">Logo</h1>
-
+    <div className="shadow-xl">
+      <nav className=" container mx-auto flex  justify-between items-center p-4 bg-white   ">
+        <img src={logo} alt="" />
         <ul className="flex gap-6">
-            <li className="font-semibold text-white text-lg"><a href="#" className="hover:text-blue-900">Home</a>{" "}</li>
-            <li className="font-semibold text-white text-lg"><a href="#" className="hover:text-blue-900">Features</a>{" "}</li>
-            <li className="font-semibold text-white text-lg"><a href="#" className="hover:text-blue-900">Community</a>{" "}</li>
-            <li className="font-semibold text-white text-lg"><a href="#" className="hover:text-blue-900">Blog</a>{" "}</li>
-            <li className="font-semibold text-white text-lg"><a href="#" className="hover:text-blue-900">Contact</a>{" "}</li>
+          <li className="font-semibold     text-lg">
+            <a href="#" className="hover:text-navbar">
+              Home
+            </a>{" "}
+          </li>
+          <li className="font-semibold     text-lg">
+            <a href="#" className="hover:text-navbar">
+              Features
+            </a>{" "}
+          </li>
+          <li className="font-semibold     text-lg">
+            <a href="#" className="hover:text-navbar">
+              Community
+            </a>{" "}
+          </li>
+          <li className="font-semibold     text-lg">
+            <a href="#" className="hover:text-navbar">
+              Blog
+            </a>{" "}
+          </li>
+          <li className="font-semibold     text-lg">
+            <a href="#" className="hover:text-navbar">
+              Contact
+            </a>{" "}
+          </li>
+        </ul>
 
-        </ul>   
-
-        <button className="bg-inherit flex justify-end"> Register Now
-        </button>
+        <button className="bg-inherit flex justify-end font-semibold text-lg hover:text-navbar"> Register Now</button>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
